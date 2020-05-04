58306bd54385efa0fb9ba00252bbe22a57056104
Leonixidas <leanderdehertogh@hotmail.com>
2020-05-04 15:47:02 +0200

    Changing the Logging system a bit

--- Imp/src/Imp/Log.h
+++ Imp/src/Imp/Log.h
@@ -32,3 +32,15 @@ namespace Imp
 	};
 }
 
+#ifdef IMP_DEBUG
+#define IMP_INFO(x) Imp::Log::Info(x)
+#define IMP_SUCCEED(x) Imp::Log::Succeed(x)
+#define IMP_WARNING(x) Imp::Log::Warn(x)
+#define IMP_ERROR(x) Imp::Log::Error(x)
+#else
+#define IMP_INFO(x)
+#define IMP_SUCCEED(x)
+#define IMP_WARNING(x)
+#define IMP_ERROR(x)
+#endif
+
